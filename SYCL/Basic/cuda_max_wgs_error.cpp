1813869a7012d353e1035c82eca362bcf0e1cb9b
Tadej Ciglarič <tadej.ciglaric@codeplay.com>
2022-03-28 10:57:52 +0100

    fix a typo and format

--- SYCL/Basic/cuda_max_wgs_error.cpp
+++ SYCL/Basic/cuda_max_wgs_error.cpp
@@ -1,7 +1,7 @@
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 //
-// REQUIRE: cuda
+// REQUIRES: cuda
 
 #include <numeric>
 #include <sycl/sycl.hpp>
@@ -11,14 +11,17 @@ using namespace sycl;
 const size_t lsize = 32;
 const size_t max_x = (1ull << 31ull) - 1ull;
 const size_t max_yz = 65535;
-const std::string expected_msg = "Number of work-groups exceed limit for dimension ";
+const std::string expected_msg =
+    "Number of work-groups exceed limit for dimension ";
 
 template <int N>
-void check(sycl::range<N> global, sycl::range<N> local, bool expect_fail = false) {
+void check(sycl::range<N> global, sycl::range<N> local,
+           bool expect_fail = false) {
   queue q;
   try {
     q.submit([&](sycl::handler &cgh) {
-      cgh.parallel_for(sycl::nd_range<N>(global, local), [=](sycl::nd_item<N> item) {});
+      cgh.parallel_for(sycl::nd_range<N>(global, local),
+                       [=](sycl::nd_item<N> item) {});
     });
   } catch (sycl::nd_range_error e) {
     if (expect_fail) {
@@ -38,12 +41,16 @@ int main() {
   check(sycl::range<2>(1, max_x * lsize), sycl::range<2>(1, lsize));
   check(sycl::range<2>(1, (max_x + 1) * lsize), sycl::range<2>(1, lsize), true);
   check(sycl::range<2>(max_yz * lsize, 1), sycl::range<2>(lsize, 1));
-  check(sycl::range<2>((max_yz + 1) * lsize, 1), sycl::range<2>(lsize, 1), true);
+  check(sycl::range<2>((max_yz + 1) * lsize, 1), sycl::range<2>(lsize, 1),
+        true);
 
   check(sycl::range<3>(1, 1, max_x * lsize), sycl::range<3>(1, 1, lsize));
-  check(sycl::range<3>(1, 1, (max_x + 1) * lsize), sycl::range<3>(1, 1, lsize), true);
+  check(sycl::range<3>(1, 1, (max_x + 1) * lsize), sycl::range<3>(1, 1, lsize),
+        true);
   check(sycl::range<3>(1, max_yz * lsize, 1), sycl::range<3>(1, lsize, 1));
-  check(sycl::range<3>(1, (max_yz + 1) * lsize, 1), sycl::range<3>(1, lsize, 1), true);
+  check(sycl::range<3>(1, (max_yz + 1) * lsize, 1), sycl::range<3>(1, lsize, 1),
+        true);
   check(sycl::range<3>(max_yz * lsize, 1, 1), sycl::range<3>(lsize, 1, 1));
-  check(sycl::range<3>((max_yz + 1) * lsize, 1, 1), sycl::range<3>(lsize, 1, 1), true);
+  check(sycl::range<3>((max_yz + 1) * lsize, 1, 1), sycl::range<3>(lsize, 1, 1),
+        true);
 }
