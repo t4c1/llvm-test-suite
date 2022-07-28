a688b693b9ebca25f94b77edc697b02e63b5bd2f
Tadej Ciglarič <tadej.ciglaric@codeplay.com>
2022-07-28 13:36:28 +0100

    improve barrier test

--- SYCL/Basic/barrier_order.cpp
+++ SYCL/Basic/barrier_order.cpp
@@ -11,28 +11,61 @@ int main() {
   sycl::device dev{sycl::default_selector{}};
   sycl::queue q{dev};
 
-  int *x = sycl::malloc_shared<int>(1, q);
-  int *y = sycl::malloc_shared<int>(1, q);
+  unsigned long long *x = sycl::malloc_shared<unsigned long long>(1, q);
+
+  int error = 0;
+
+  // test barrier without arguments
   *x = 0;
-  *y = 0;
-
-  q.single_task<class kernel1>([=] { *x = 1; });
-
-  q.ext_oneapi_submit_barrier();
-
-  q.single_task<class kernel2>([=] {
-    if (*x == 1) {
-      *y = 2;
-    }
-  });
+  for (int i = 0; i < 64; i++) {
+    q.parallel_for(1024, [=](cl::sycl::id<1> ID) {
+      // do some busywork
+      float y = *x;
+      for (int j = 0; j < 10000; j++) {
+        y = sycl::cos(y);
+      }
+      // update the value
+      if (ID.get(0) == 0)
+        *x *= 2;
+    });
+    q.ext_oneapi_submit_barrier();
+    q.parallel_for(1024, [=](cl::sycl::id<1> ID) {
+      if (ID.get(0) == 0)
+        *x += 1;
+    });
+    q.ext_oneapi_submit_barrier();
+  }
 
   q.wait_and_throw();
+  error |= (*x != (unsigned long long)-1);
 
-  int error = (*x != 1 || *y != 2) ? 1 : 0;
+  // test barrier when events are passed arguments
+  *x = 0;
+  for (int i = 0; i < 64; i++) {
+    sycl::event e = q.parallel_for(1024, [=](cl::sycl::id<1> ID) {
+      // do some busywork
+      float y = *x;
+      for (int j = 0; j < 10000; j++) {
+        y = sycl::cos(y);
+      }
+      // update the value
+      if (ID.get(0) == 0)
+        *x *= 2;
+    });
+    q.ext_oneapi_submit_barrier({e});
+    e = q.parallel_for(1024, [=](cl::sycl::id<1> ID) {
+      if (ID.get(0) == 0)
+        *x += 1;
+    });
+    q.ext_oneapi_submit_barrier({e});
+  }
+
+  q.wait_and_throw();
+  error |= (*x != (unsigned long long)-1);
+
   std::cout << (error ? "failed\n" : "passed\n");
 
   sycl::free(x, q);
-  sycl::free(y, q);
 
   return error;
 }
